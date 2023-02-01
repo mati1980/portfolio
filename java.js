020625633abec6ed8d0d7053e1e6e6f3cd87f29c
Matilde Hadrup Mikkelsen <mati1980@kea.stud.dk>
2023-02-01 21:32:42 +0100

    burgermenu og tema 4 mm

--- java.js
+++ java.js
@@ -1,5 +1,12 @@
 window.addEventListener("load", sidenVises);
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav_menu");
+
 function sidenVises() {
   console.log("sidenVises");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 }
